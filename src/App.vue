6130e709cc1271750f4121c9f18da75e651be60c
Aydar <sitdikov@protonmail.ch>
2020-10-17 18:15:11 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="max-height:300px">
+    <amplify-chatbot v-bind:chatbotConfig="chatbotConfig"></amplify-chatbot>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Interactions } from "aws-amplify";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    chatbotConfig: {
+      bot: "OrderFlowers_dev",
+      clearComplete: false,
+    },
+  }),
+  mounted() {
+    Interactions.onComplete("OrderFlowers_dev", this.handleComplete);
+  },
+  methods: {
+    handleComplete(err, confirmation) {
+      if (err) {
+        alert("bot conversation failed");
+        return;
+      }
+      alert("done: " + JSON.stringify(confirmation, null, 2));
+
+      return "Thank you! What would you like to do next?";
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Amplify Modules, Plugins, and aws exports file
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import awsconfig from "./aws-exports";
+
+// Configure Amplify in project
+Amplify.configure(awsconfig);
+
+// Configure vue to use plugins and modules
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
